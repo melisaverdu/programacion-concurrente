357010c7ec4ce64273808af6b6fece353713e1aa
melisa <melisa.verdu@mi.unc.edu.ar>
2026-04-21 21:44:52 -0300

    Enhance comments in FileClock class to clarify thread interruption handling

--- concurrency-cookbook/Sources/Clase4_2026_FileClock/FileClock.java
+++ concurrency-cookbook/Sources/Clase4_2026_FileClock/FileClock.java
@@ -11,9 +11,14 @@ public class FileClock implements Runnable {
 				TimeUnit.SECONDS.sleep(1); // cuano interrumpo un hilo que esta en sleep, en ese preciso instante lo despierto en el moemnto en el que este.
 			} catch (InterruptedException e) {
 				System.out.printf("The FileClock has been interrupted\n");
-				return;
+				return; // el return lo mata y sale del método run, lo que hace que el hilo termine su ejecución
+				//si el return no estuviera, el hilo seguiría ejecutándose y seguiría imprimiendo la fecha cada segundo, incluso después de haber sido interrumpido.
 			}
-			
+			/*
+			si en vez de hacer un try catch y solo quisiera poner el hilo a dormir -> no podría
+			Porque el método sleep lanza una excepción, y si no la manejo, el código no compilaría. Por eso es necesario el try catch para manejar la posible 
+			interrupción del hilo mientras está durmiendo.
+			*/
 		}
 	}
 }
